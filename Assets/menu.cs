90339fb8f18b68a6270bd0bb9926c982bef68753
wilczek10 <wilczynski.gabrys@gmail.com>
2023-12-02 14:45:00 +0100

    koniec

--- Assets/menu.cs
+++ Assets/menu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Dziwna Scena");
+        SceneManager.LoadScene("ScenaCweli");
     }
 
     public void GoToSettingsMenu()
